215cd3b407147898a3d341034a5faae04991681e
王志轩 <wangzhixuan16888@163.com>
2025-12-02 11:17:20 +0800

    01-hello-world

---
+++ _01-hello-world/src/main/java/cn/abaabaai/Application.java
@@ -0,0 +1,16 @@
+package cn.abaabaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+/**
+ * 应用入口：启动 Spring Boot 应用
+ */
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
